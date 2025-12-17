c27f1e0de0db4938f8dfd02f79d6257189e29e5b
FoxyArt <foxyart321@gmail.com>
2025-12-17 15:55:08 +0200

    post lists and detail views

--- DEVOPS_5_PRACTICE/myblog/blog/views.py
+++ DEVOPS_5_PRACTICE/myblog/blog/views.py
@@ -1,5 +1,14 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-def home(request):
-    return HttpResponse("Перевірка")
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("Post not found.")
+    return render(request, 'blog/post/detail.html', {'post': post})
