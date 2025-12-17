5208848a966373212acb134c96aa192eb8867fad
FoxyArt <foxyart321@gmail.com>
2025-12-17 15:45:26 +0200

    added statuses

--- DEVOPS_5_PRACTICE/myblog/blog/models.py
+++ DEVOPS_5_PRACTICE/myblog/blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -14,7 +19,12 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
-
+        
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+)
 
     def __str__(self):
         return self.title
